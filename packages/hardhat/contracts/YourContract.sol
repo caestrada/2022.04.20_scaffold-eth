f7ba20633e0a914bea0b694660adcb484a316007
Carlos A. Estrada <carlos.estrada@coretechnologies.io>
2022-04-25 20:57:56 -0400

    SBE loops

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -5,6 +5,7 @@ import "hardhat/console.sol";
 
 contract YourContract {
 
+    // If/Else ---------------------------------------------------
     function foo(uint x) public pure returns (uint) {
         if (x < 10) {
             return 0;
@@ -23,5 +24,31 @@ contract YourContract {
 
         // shorthand way to write if / else statement
         return _x < 10 ? 1 : 2;
+    }
+    // -----------------------------------------------------------
+
+    // While Loops -----------------------------------------------
+    // Solidity supports for, while, and do while loops.
+    // Don't write loops that are unbounded as this can hit the gas limit, causing your transaction to fail.
+    // For the reason above, while and do while loops are rarely used.
+    function loop() public {
+        // for loop
+        for (uint i=0; i<10; i++) {
+            if (i == 3) {
+                // Skip to next iteration with continue
+                continue;
+            }
+
+            if (i == 5) {
+                // Exit loop with break
+                break;
+            }
+        }
+
+        // while loop
+        uint j;
+        while (j < 10) {
+            j++;
+        }
     }
 }
