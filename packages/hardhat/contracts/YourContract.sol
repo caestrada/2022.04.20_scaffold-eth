ed3e1470abedf5547a875742a09ffc43b4a9fe57
Carlos A. Estrada <carlos.estrada@coretechnologies.io>
2022-04-26 22:21:26 -0400

    SBE Array 2

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,26 +2,40 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol"; 
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
+// Array 2
+// Remove array element by shifting elements from right to left
 contract YourContract {
+    // [1, 2, 3] -- remove(1) --> [1, 3, 3] --> [1, 3]
+    // [1, 2, 3, 4, 5, 6] -- remove(2) --> [1, 2, 4, 5, 6, 6] --> [1, 2, 4, 5, 6]
+    // [1, 2, 3, 4, 5, 6] -- remove(0) --> [2, 3, 4, 5, 6, 6] --> [2, 3, 4, 5, 6]
+    // [1] -- remove(0) --> [1] --> []
 
-  event SetPurpose(address sender, string purpose);
+    uint[] public arr;
 
-  string public purpose = "Building Unstoppable Apps!!!";
+    function remove(uint _index) public {
+        require(_index < arr.length, "index out of bound");
 
-  constructor() payable {
-    // what should we do on deploy?
-  }
+        for (uint i = _index; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr.pop();
+    }
 
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      emit SetPurpose(msg.sender, purpose);
-  }
+    function test() external {
+        arr = [1, 2, 3, 4, 5];
+        remove(2);
+        // [1, 2, 4, 5]
+        assert(arr[0] == 1);
+        assert(arr[1] == 2);
+        assert(arr[2] == 4);
+        assert(arr[3] == 5);
+        assert(arr.length == 4);
 
-  // to support receiving ETH by default
-  receive() external payable {}
-  fallback() external payable {}
+        arr = [1];
+        remove(0);
+        // []
+        assert(arr.length == 0);
+    }
+
 }
